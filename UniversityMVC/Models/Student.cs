8b432c5526a261309130b1a3935dc310d1208a4c
Mario <das16005@byui.edu>
2019-11-21 17:22:13 -0700

    Add project files.

---
+++ UniversityMVC/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using UniversityMVC.Models.SchoolViewModels;
+
+namespace UniversityMVC.Models
+{
+    public class Student : Person
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
